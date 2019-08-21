f2380d70d8aa75b75b2fcf6578d2740e2235381a
Sergey Zinchenko <s.zinchenko@e-mba.ru>
2019-08-21 14:36:38 +0300

    Add params to recurring interface

--- src/contracts/recurring/RecurringPayment.php
+++ src/contracts/recurring/RecurringPayment.php
@@ -19,13 +19,16 @@ interface RecurringPayment
      * Initialize recurring payment
      *
      * @param string $token
+     * @param string $accountId
      * @param float  $amount
      * @param string $description
      * @param string $currency
      *
+     * @param array  $extraParams
+     *
      * @return bool
      */
-    public function initPayment(string $token, float $amount, string $description, string $currency = PayService::CURRENCY_RUR_ISO): bool;
+    public function initPayment(string $token, string $accountId, float $amount, string $description, string $currency = PayService::CURRENCY_RUR_ISO, array $extraParams = []): bool;
 
     /**
      * Remember payment fo recurring payments
